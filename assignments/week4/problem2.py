92618ce8eeec4ab5e561f67063f240c214bbd8cd
MJNowak997 <m.j.nowak997@gmail.com>
2025-06-24 06:03:34 +0000

    simplifyweek4prob2

--- assignments/week4/problem2.py
+++ assignments/week4/problem2.py
@@ -1,10 +1,10 @@
 def print_triangular_numbers(n):
     x = 0
-    p = 0
+    r = 0
     for i in range(1, n+1):
-        r = 1 + p + x
+        r = 1 + r + x
         x = x + 1
-        p = r
+        r = r
         print(i, "    ", r)
 
 print_triangular_numbers(5)
